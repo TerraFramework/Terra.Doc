00d3d82539864545f58bbf37c623607370f7fc35
ozgeg <ozge.gundogdu35@gmail.com>
2017-04-03 22:00:22 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('crawler/src'))
+#sys.path.insert(0, os.path.abspath('crawler/src'))
 
 # -- General configuration -----------------------------------------------------
 
